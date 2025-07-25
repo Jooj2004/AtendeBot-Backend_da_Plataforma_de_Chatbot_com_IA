c73c538900f0aad80417145b9990e073a44da49b
Josdegar <josdegarfsantos@gmail.com>
2025-07-25 10:16:36 -0300

    tests in service company finish

--- src/services/company.test.ts
+++ src/services/company.test.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs"
 import { prisma } from "../libs/prisma"
-import { createCompany, getCompanyByEmail, updateCompany } from "./company"
+import { createCompany, deleteCompany, getCompanyByEmail, updateCompany, updateEmail, updatePassword } from "./company"
 
 const company = {
     name: "Empresa Teste",
@@ -9,8 +9,11 @@ const company = {
     password: "SenhaSegura123#",
     description: "Empresa fictícia para teste"
 }
+const newEmail = "novoEmail@gmail.com"
 
-describe("Testing services of company.ts", () => {
+describe("Testing services of company.ts", async () => {
+    const getCompany = await getCompanyByEmail(company.email)
+
     beforeAll(async () => {
         await prisma.company.deleteMany()
     })
@@ -31,15 +34,11 @@ describe("Testing services of company.ts", () => {
     })
 
     it("Should service getCompanyByEmail success", async () => {
-       const getCompany = await getCompanyByEmail(company.email)
-
         expect(getCompany?.email).toBe(company.email)
         expect(getCompany?.CNPJ).toBe(company.CNPJ) 
     })
 
     it("Should service updateCompany success", async () => {
-        const getCompany = await getCompanyByEmail(company.email)
-
         const newData = {
             name: "Novo nome",
             description: "Nova descrição"
@@ -51,5 +50,43 @@ describe("Testing services of company.ts", () => {
         expect(newCompany.id).toBe(getCompany?.id)
         expect(newCompany.name).not.toBe(company.name)
         expect(newCompany.name).toBe(newData.name)
+    })
+
+    it("Should service updateEmail success", async () => {
+        const newEmailCompany = await updateEmail(getCompany?.id as string, newEmail)
+
+        expect(newEmailCompany).toBeDefined()
+        expect(newEmailCompany.id).toBe(getCompany?.id)
+        expect(getCompany?.email).not.toBe(newEmailCompany.email)
+        expect(newEmailCompany.email).toBe(newEmail)
+        expect(newEmailCompany.verification).toBe(false)
+    })
+
+    it("Should service updatePassword success", async () => {
+        const newPass = "novaSenha"
+
+        const getCompany = await getCompanyByEmail(newEmail)
+        console.log(getCompany?.id)
+
+        const hash = await bcrypt.hash(newPass,10)
+
+        const newPassCompany = await updatePassword(getCompany?.id as string, hash)
+
+        expect(newPassCompany).toBeDefined()
+        expect(newPassCompany.id).toBe(getCompany?.id)
+        expect(newPassCompany.password).not.toBe(newPass)
+        expect(newPassCompany.password).toBe(hash)
+    })
+
+    it("Should service updatePassword success", async () => {
+        const getCompany = await getCompanyByEmail(newEmail)
+
+        const finish = await deleteCompany(getCompany?.id as string)
+
+        expect(finish).toBe(true)
+
+        const getCompany2 = await getCompanyByEmail(newEmail)
+
+        expect(getCompany2).not.toBeDefined
     })
 })
