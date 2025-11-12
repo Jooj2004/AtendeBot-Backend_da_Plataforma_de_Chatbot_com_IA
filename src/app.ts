bc51880d01e0c99db848f7bd6712647a6f73dc59
Josdegar <josdegarfsantos@gmail.com>
2025-11-12 12:31:42 -0300

    fix?  para aceitar netfly

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,17 @@ import chatRouter from './routes/chatbot-public-routes'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: [
+        "http://localhost:3000",
+        "https://atendebot-frontend.netlify.app"
+    ],
+    credentials: true,
+}))
 app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(express.static(path.join(__dirname,'.../public')))
+app.use(express.static(path.join(__dirname,'../public')))
 app.use(mainRouter)
 app.use(chatRouter)
 
